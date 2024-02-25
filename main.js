d6dc576694318fb1132637330f71d86702ce6d05
Moosanoor24 <159759573+Moosanoor24@users.noreply.github.com>
2024-02-25 10:34:54 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("HELLO WORLD");
 console.log("how are u?");
 console.log("My self MOOSA");
 console.log("I m 20year");
-console.log("I have done inter");
+console.log("I had done inter");
 console.log("Now a days my university going on as well as I am learning about typescript");
 console.log("INSALLAH as soon i will acheive my all goals/desires and lets become a panther of typescript");
 console.log("THANK YOU SO MUCH FOR GIVING ME YOUR PRECIOUS TIME");
